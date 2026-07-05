f5637c1d90270f7cdcd28c514c11ca36ac6ec01d
kapepa <kapepayx@gmail.com>
2026-07-05 20:12:36 +0300

    refactor(auth): redesign login and register forms - Improve form layouts and styling - Add better error handling and validation feedback - Update login button with dropdown menu - Enhance user experience with loading states - Refactor form hooks with improved error messages

--- src/features/auth/model/hooks/use-login-form.ts
+++ src/features/auth/model/hooks/use-login-form.ts
@@ -5,18 +5,23 @@ import { toast } from "sonner";
 import { loginFormSchema, LoginFormValues, } from "../schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useAuthStore } from "../auth-store";
+import { requestAuthLogin } from "../../api";
+import { ROUTES, useRouter } from "@/shared";
 
 export const useLoginForm = () => {
-  const tToast = useTranslations("auth.toast")
-  const tErrors = useTranslations("auth.errors")
+  const tToast = useTranslations("auth.form.toast")
+  const tErrors = useTranslations("auth.form.errors")
   const [isSubmitting, startSubmitTransition] = useTransition()
+  const router = useRouter()
   const { setUser } = useAuthStore();
 
   const form = useForm<LoginFormValues>({
     resolver: zodResolver(loginFormSchema(tErrors)),
+    mode: "onChange",
+    reValidateMode: "onChange",
     defaultValues: {
-      email: "",
-      password: ""
+      email: "karma@gmail.com",
+      password: "123456A!",
     },
   })
 
@@ -24,10 +29,11 @@ export const useLoginForm = () => {
     (values: LoginFormValues) => {
       startSubmitTransition(async () => {
         try {
-          console.log(values)
+          const { } = await requestAuthLogin({ data: values })
 
           toast.success(tToast("auth-login-success"))
           form.reset()
+          router.push(ROUTES.PARISHES)
         } catch (error) {
           console.error(error)
           toast.error(tToast("auth-login-error"))
@@ -37,6 +43,10 @@ export const useLoginForm = () => {
     [tToast, form]
   )
 
+  const onReset = useCallback(() => {
+    form.reset(undefined, { keepDefaultValues: true });
+  }, [form]);
+
   const handleSubmit = useMemo(() => form.handleSubmit(onSubmit), [form, onSubmit])
 
   return useMemo(
@@ -44,7 +54,8 @@ export const useLoginForm = () => {
       form,
       isSubmitting,
       onSubmit: handleSubmit,
+      onReset,
     }),
-    [form, isSubmitting, handleSubmit]
+    [form, isSubmitting, handleSubmit, onReset]
   )
 }
