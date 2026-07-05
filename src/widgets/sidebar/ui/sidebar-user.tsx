3472d2bb423d86770499aae9c5ea8afaf2890675
kapepa <kapepayx@gmail.com>
2026-07-05 20:13:09 +0300

    feat(sidebar): use hydrated user to prevent flicker
    - Replace useAuthStore with useHydratedUser hook
    - Show server-rendered user data immediately
    - Eliminate UI flicker during hydration

--- src/widgets/sidebar/ui/sidebar-user.tsx
+++ src/widgets/sidebar/ui/sidebar-user.tsx
@@ -1,25 +1,24 @@
+"use client"
+
 import { LoginButton } from "@/features"
+import { useHydratedUser } from "@/features/auth/model/hooks"
 import { cn, ProfileAvatar } from "@/shared"
-import { memo } from "react"
 
 interface SidebarUserProps {
   className?: string
 }
 
-export const SidebarUser = memo(
-  ({ className }: SidebarUserProps) => {
-    const MocName = "Cate"
-    const MocUrl = "/images/avatar-face.jpg"
+export const SidebarUser = ({ className }: SidebarUserProps) => {
+  const user = useHydratedUser()
 
-    return (
-      <div className={cn("", className)}>
-        <div className="relative size-30">
-          <ProfileAvatar url={MocUrl} name={MocName} />
-          <div className="absolute right-0 bottom-0 transform translate-x-1/4 translate-y-1/4">
-            <LoginButton />
-          </div>
+  return (
+    <div className={cn("", className)}>
+      <div className="relative size-30">
+        <ProfileAvatar url={user?.imageUrl} name={user?.name} />
+        <div className="absolute right-0 bottom-0 transform translate-x-1/4 translate-y-1/4">
+          <LoginButton />
         </div>
       </div>
-    )
-  }
-)
+    </div>
+  )
+}
