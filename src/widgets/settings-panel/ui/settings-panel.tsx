1fac067a7629252a9eb28d96d0ede7a0aca4e1ec
kapepa <kapepayx@gmail.com>
2026-07-19 17:53:51 +0300

    UI - Fix scroll area and layout issues

--- src/widgets/settings-panel/ui/settings-panel.tsx
+++ src/widgets/settings-panel/ui/settings-panel.tsx
@@ -1,23 +1,20 @@
 "use client"
 
-import { AvatarUpload, ChangePasswordForm, DeleteAccount, InitialUserContext, RoleSelector } from "@/features"
-import { ScrollArea } from "@/shared";
+import { AvatarUpload, ChangePasswordForm, DeleteAccount, InitialUserContext, RoleSelector, useHydratedUser } from "@/features"
 import { useContext } from "react";
 
 export const SettingsPanel = () => {
-  const user = useContext(InitialUserContext);
+  const user = useHydratedUser();
 
   if (!user) return null;
 
   return (
-    <ScrollArea className="flex-1 min-h-0">
-      <div className="grid grid-cols-1 place-items-center gap-y-6 m-auto pb-6 md:pb-16">
-        <AvatarUpload user={user} />
-        <ChangePasswordForm className="max-w-xl" />
-        <RoleSelector className="max-w-xl" user={user} />
-        <DeleteAccount user={user} />
-      </div>
-    </ScrollArea>
+    <div className="grid grid-cols-1 place-items-center gap-y-6 m-auto pb-6 md:pb-16">
+      <AvatarUpload user={user} />
+      <ChangePasswordForm className="max-w-xl" />
+      <RoleSelector className="max-w-xl" user={user} />
+      <DeleteAccount user={user} />
+    </div>
   )
 }
 
--- src/widgets/users-list/ui/users-list.tsx
+++ src/widgets/users-list/ui/users-list.tsx
@@ -42,9 +42,9 @@ export const UsersList = ({ className, initialUsers, initialHasMore }: UsersList
   )
 
   return (
-    <div className="flex-1 min-h-0 flex flex-col">
-      <ScrollArea className="flex-1 min-h-0">
-        <div className={cn("flex flex-col gap-3 max-w-lg lg:max-w-full m-auto pb-6 md:pb-16", className)}>
+    <div className="flex-1 min-h-0 flex flex-col w-full">
+      <ScrollArea className="flex-1 min-h-0 w-full mx-auto max-w-lg lg:max-w-full">
+        <div className={cn("flex flex-col gap-3 pb-6 md:pb-16", className)}>
           {
             users.map((user) => (
               <UserCard
