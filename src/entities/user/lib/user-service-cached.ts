5aebed587d0f24bf9f80063080c9e78c73fcb0db
kapepa <kapepayx@gmail.com>
2026-07-24 20:22:52 +0300

    refactor(user): update user service caching strategy

--- src/entities/user/lib/user-service-cached.ts
+++ src/entities/user/lib/user-service-cached.ts
@@ -1,7 +1,7 @@
 import { unstable_cache } from 'next/cache';
 import { getFilteredUsers } from './user-service';
 import { FetchUsers } from '../model';
-import { CACHE_ENTITIES, CACHE_REVALIDATE, CACHE_TAGS, createCacheKey } from '@/shared/server';
+import { CACHE_ENTITIES, CACHE_REVALIDATE, createCacheKey } from '@/shared/server';
 
 export const getFilteredUsersCached = (params: FetchUsers) => {
   const cacheKey = createCacheKey(
